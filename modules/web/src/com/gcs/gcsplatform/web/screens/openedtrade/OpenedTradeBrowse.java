e8bcafc7d5f26d0bb02559576c890c816d6f5dd5
Panevin <panevin.di@haulmont.com>
2021-07-28 10:36:36 +0400

    Close and close/reopen screen logic

--- modules/web/src/com/gcs/gcsplatform/web/screens/openedtrade/OpenedTradeBrowse.java
+++ modules/web/src/com/gcs/gcsplatform/web/screens/openedtrade/OpenedTradeBrowse.java
@@ -1,5 +1,8 @@
 package com.gcs.gcsplatform.web.screens.openedtrade;
 
+import javax.inject.Inject;
+
+import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.*;
 import com.gcs.gcsplatform.entity.trade.OpenedTrade;
 
@@ -8,4 +11,14 @@ import com.gcs.gcsplatform.entity.trade.OpenedTrade;
 @LookupComponent("openedTradesTable")
 @LoadDataBeforeShow
 public class OpenedTradeBrowse extends StandardLookup<OpenedTrade> {
+
+    @Inject
+    private CollectionLoader<OpenedTrade> openedTradesDl;
+
+    @Install(to = "openedTradesTable.edit", subject = "afterCloseHandler")
+    private void openedTradesTableEditAfterCloseHandler(AfterCloseEvent afterCloseEvent) {
+        if (afterCloseEvent.closedWith(StandardOutcome.COMMIT)) {
+            openedTradesDl.load();
+        }
+    }
 }
--- modules/web/src/com/gcs/gcsplatform/web/screens/openedtrade/OpenedTradeEdit.java
+++ modules/web/src/com/gcs/gcsplatform/web/screens/openedtrade/OpenedTradeEdit.java
@@ -1,6 +1,20 @@
 package com.gcs.gcsplatform.web.screens.openedtrade;
 
+import java.util.Date;
+import javax.inject.Inject;
+
+import com.gcs.gcsplatform.service.CloseTradeService;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.View;
+import com.haulmont.cuba.core.global.ViewBuilder;
+import com.haulmont.cuba.gui.Dialogs;
+import com.haulmont.cuba.gui.Notifications;
+import com.haulmont.cuba.gui.app.core.inputdialog.DialogActions;
+import com.haulmont.cuba.gui.app.core.inputdialog.DialogOutcome;
+import com.haulmont.cuba.gui.app.core.inputdialog.InputParameter;
 import com.haulmont.cuba.gui.components.Button;
+import com.haulmont.cuba.gui.model.InstanceContainer;
+import com.haulmont.cuba.gui.model.InstanceLoader;
 import com.haulmont.cuba.gui.screen.*;
 import com.gcs.gcsplatform.entity.trade.OpenedTrade;
 
@@ -8,15 +22,67 @@ import com.gcs.gcsplatform.entity.trade.OpenedTrade;
 @UiDescriptor("opened-trade-edit.xml")
 @EditedEntityContainer("openedTradeDc")
 @LoadDataBeforeShow
+@DialogMode(forceDialog = true)
 public class OpenedTradeEdit extends StandardEditor<OpenedTrade> {
+
+    @Inject
+    private DataManager dataManager;
+    @Inject
+    private Dialogs dialogs;
+    @Inject
+    private Notifications notifications;
+    @Inject
+    private MessageBundle messageBundle;
+    @Inject
+    private CloseTradeService closeTradeService;
+
+    @Inject
+    private InstanceContainer<OpenedTrade> openedTradeDc;
 
     @Subscribe("closeReopenTradeBtn")
     public void onCloseReopenTradeBtnClick(Button.ClickEvent event) {
-
+        dialogs.createInputDialog(this)
+                .withCaption(messageBundle.getMessage("closeReopenTradeDialog.caption"))
+                .withParameter(InputParameter.dateTimeParameter("maturityDate")
+                        .withCaption(messageBundle.getMessage("maturityDate"))
+                        .withRequired(true))
+                .withActions(DialogActions.OK_CANCEL)
+                .withCloseListener(inputDialogCloseEvent -> {
+                    if (inputDialogCloseEvent.closedWith(DialogOutcome.OK)) {
+                        Date maturityDate = inputDialogCloseEvent.getValue("maturityDate");
+                        closeTradeService.closeReopen(getEditedEntity(), maturityDate);
+                        OpenedTrade openedTrade = dataManager.reload(getEditedEntity(), ViewBuilder.of(
+                                OpenedTrade.class)
+                                .add("trade", View.LOCAL)
+                                .addView(View.LOCAL)
+                                .build());
+                        openedTradeDc.setItem(openedTrade);
+                        notifications.create(Notifications.NotificationType.TRAY)
+                                .withDescription(messageBundle.getMessage("tradeClosedAndReopened"))
+                                .show();
+                    }
+                })
+                .show();
     }
 
     @Subscribe("closeTradeBtn")
     public void onCloseTradeBtnClick(Button.ClickEvent event) {
-
+        dialogs.createInputDialog(this)
+                .withCaption(messageBundle.getMessage("closeTradeDialog.caption"))
+                .withParameter(InputParameter.dateTimeParameter("maturityDate")
+                        .withCaption(messageBundle.getMessage("maturityDate"))
+                        .withRequired(true))
+                .withActions(DialogActions.OK_CANCEL)
+                .withCloseListener(inputDialogCloseEvent -> {
+                    if (inputDialogCloseEvent.closedWith(DialogOutcome.OK)) {
+                        Date maturityDate = inputDialogCloseEvent.getValue("maturityDate");
+                        closeTradeService.close(getEditedEntity(), maturityDate);
+                        close(StandardOutcome.COMMIT);
+                        notifications.create(Notifications.NotificationType.TRAY)
+                                .withDescription(messageBundle.getMessage("tradeClosed"))
+                                .show();
+                    }
+                })
+                .show();
     }
 }
