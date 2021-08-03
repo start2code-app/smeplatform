82e0a0a161782ba2ff97744d1709f43bc6c620b8
Panevin <panevin.di@haulmont.com>
2021-08-03 19:14:06 +0400

    Drop counterparty columns

---
+++ modules/core/db/update/mysql/21/08/210803-2-updateCounterparty.sql
@@ -0,0 +1,16 @@
+alter table GCSPLATFORM_COUNTERPARTY change column SPEC_UST SPEC_UST__U42536 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column SPEC_SSA SPEC_SSA__U81868 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column SPEC_GILT SPEC_GILT__U40962 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column SPEC_EM SPEC_EM__U84638 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column SPEC_EGBPIGS SPEC_EGBPIGS__U14911 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column SPEC_EGB SPEC_EGB__U74772 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column SPEC_CORP SPEC_CORP__U04204 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column SPEC_ABS SPEC_ABS__U34431 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column GC_UST GC_UST__U47991 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column GC_SSA GC_SSA__U01605 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column GC_GILT GC_GILT__U22404 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column GC_EM GC_EM__U34753 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column GC_EGBPIGS GC_EGBPIGS__U75419 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column GC_EGB GC_EGB__U49925 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column GC_CORP GC_CORP__U35596 varchar(10)^
+alter table GCSPLATFORM_COUNTERPARTY change column GC_ABS GC_ABS__U96253 varchar(10)^
