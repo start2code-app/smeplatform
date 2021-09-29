7547b99d9ef5b9535ba7e41453278c821b5d8075
Panevin <panevin.di@haulmont.com>
2021-09-29 17:25:44 +0400

    CBE-33: Javadoc

--- modules/core/src/com/gcs/gcsplatform/service/fxprovider/FxProviderAPI.java
+++ modules/core/src/com/gcs/gcsplatform/service/fxprovider/FxProviderAPI.java
@@ -6,5 +6,12 @@ public interface FxProviderAPI {
 
     String NAME = "gcsplatform_FxProvider";
 
+    /**
+     * Gets actual FX from external service.
+     *
+     * @param fromCurrency From currency code
+     * @param toCurrency   To currency code
+     * @return FX value
+     */
     BigDecimal getFx(String fromCurrency, String toCurrency);
 }
--- modules/global/src/com/gcs/gcsplatform/service/fx/FxScheduledUpdateService.java
+++ modules/global/src/com/gcs/gcsplatform/service/fx/FxScheduledUpdateService.java
@@ -4,5 +4,11 @@ public interface FxScheduledUpdateService {
 
     String NAME = "gcsplatform_FxScheduledUpdateService";
 
+    /**
+     * Updates FX for all currencies.
+     *
+     * Method is being used in scheduled task.
+     */
+    @SuppressWarnings("unused")
     void updateFxForAllCurrencies();
 }
