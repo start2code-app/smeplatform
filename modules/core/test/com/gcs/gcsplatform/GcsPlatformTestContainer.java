a8c15d4abd086d59437a5cef32805c986fc10384
ibrahim <ickhatib@gmail.com>
2022-05-29 20:55:58 +0100

    Initial smeplaform commit

--- modules/core/test/com/gcs/gcsplatform/GcsPlatformTestContainer.java
+++ modules/core/test/com/gcs/gcsplatform/GcsPlatformTestContainer.java
@@ -15,7 +15,8 @@ public class GcsPlatformTestContainer extends TestContainer {
                 "com.haulmont.cuba",
                 "com.haulmont.addon.helium",
                 "com.haulmont.charts",
-                "com.haulmont.reports");
+                "com.haulmont.reports",
+                "com.haulmont.addon.restapi");
         appPropertiesFiles = Arrays.asList(
                 // List the files defined in your web.xml
                 // in appPropertiesConfig context parameter of the core module
